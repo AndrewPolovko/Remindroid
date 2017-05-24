b34bf91d7bc620cd8cec6f807fcad0a0d2790ac3
AndrewPolovko <AndrewPolovko@gmail.com>
2017-05-24 18:16:20 +0300

    Initial commit

---
+++ app/src/main/java/com/epam/androidlab/remindroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.epam.androidlab.remindroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
